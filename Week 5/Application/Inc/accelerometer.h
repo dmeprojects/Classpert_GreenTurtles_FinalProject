bc7d2d4d6c9e88a7c791c20c51d5a271cb629d93
DME-Projects <79807855+dmeprojects@users.noreply.github.com>
2023-11-05 22:51:03 +0100

    Added accellerometer files

---
+++ Week 5/Application/Inc/accelerometer.h
@@ -0,0 +1,13 @@
+/*
+ * accelerometer.h
+ *
+ *  Created on: Nov 5, 2023
+ *      Author: nick_
+ */
+
+#ifndef INC_ACCELEROMETER_H_
+#define INC_ACCELEROMETER_H_
+
+
+
+#endif /* INC_ACCELEROMETER_H_ */
---
+++ Week 5/Application/Src/accelerometer.c
@@ -0,0 +1,8 @@
+/*
+ * accelerometer.c
+ *
+ *  Created on: Nov 5, 2023
+ *      Author: nick_
+ */
+
+
