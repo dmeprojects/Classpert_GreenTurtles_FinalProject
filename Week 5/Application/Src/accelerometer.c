89923b2e964d34a6791c0da4570b2581bde6632c
DME-Projects <79807855+dmeprojects@users.noreply.github.com>
2023-11-06 22:51:15 +0100

    Data looks still mangled up

--- Week 5/Application/Src/accelerometer.c
+++ Week 5/Application/Src/accelerometer.c
@@ -52,16 +52,16 @@ void EnableImu(void)
 
 	//transmitData[0] = 0;
 	//transmitData[0] = 0b10000000 | WHO_AM_I;
-	transmitData[0] = 0x01;
-	transmitData[1] = 0x80;
-	transmitData[2] = 0b01010101;
+	transmitData[0] = WHO_AM_I;
+	//transmitData[1] = 0x80;
+	//transmitData[2] = 0b01010101;
 	//transmitData[2] = WHO_AM_I;
 	transmitData[3] = 0b10000000 | WHO_AM_I;
 
 	HAL_GPIO_WritePin(GPIOE, CS_I2C_SPI_Pin, GPIO_PIN_RESET);
 
 	//Read the CTRL_REG1 register
-	HAL_SPI_Transmit(&hspi1, (uint8_t *)&transmitData, 8, 100);
+	HAL_SPI_Transmit(&hspi1, &transmitData[0], 8, 100);
 	//HAL_Delay(5);
 	//HAL_SPI_Receive(&hspi1, receivedData, 1, 100);
 
