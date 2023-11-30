b64d805ee7b72fc5fa5b13f1280aee04319d9cb7
DME-Projects <79807855+dmeprojects@users.noreply.github.com>
2023-11-30 22:31:09 +0100

    Fixed display arrangmnet

--- Final project/Firmware/Application/Inc/displayFunctions.h
+++ Final project/Firmware/Application/Inc/displayFunctions.h
@@ -13,6 +13,7 @@ void displayPutHeader (void);
 
 void displayAccelerometerValues(int32_t x, int32_t y, int32_t z);
 void displayGyroValues (int32_t gX, int32_t Gy, int32_t gZ);
+void displayRawValues (int32_t aX, int32_t gX, int32_t aY, int32_t gY, int32_t aZ, int32_t gZ);
 
 
 
--- Final project/Firmware/Application/Src/displayFunctions.c
+++ Final project/Firmware/Application/Src/displayFunctions.c
@@ -9,6 +9,7 @@
 
 #define FIRSTLINE	25
 #define LINESPACE	12
+#define SECONDCOLUMN	60
 
 #define SMALLFONT	Font_7x10
 
@@ -38,19 +39,19 @@ void displayAccelerometerValues(int32_t x, int32_t y, int32_t z)
 	uint8_t currentLine = FIRSTLINE;
 
 	//Write X:
-	sprintf(StringToDisplay, "X: %li     ", x);
+	sprintf(StringToDisplay, "aX:%li     ", x);
 	ssd1306_SetCursor(0, currentLine);
 	ssd1306_WriteString(StringToDisplay, SMALLFONT, White);
 	currentLine += LINESPACE;
 
 	//Write Y:
-	sprintf(StringToDisplay, "Y: %li    ", y);
+	sprintf(StringToDisplay, "aY:%li    ", y);
 	ssd1306_SetCursor(0, currentLine);
 	ssd1306_WriteString(StringToDisplay, SMALLFONT, White);
 	currentLine += LINESPACE;
 
 	//Write Z:
-	sprintf(StringToDisplay, "Z: %li     ", z);
+	sprintf(StringToDisplay, "aZ:%li     ", z);
 	ssd1306_SetCursor(0, currentLine);
 	ssd1306_WriteString(StringToDisplay, SMALLFONT, White);
 
@@ -65,23 +66,67 @@ void displayGyroValues (int32_t gX, int32_t gY, int32_t gZ)
 	uint8_t currentLine = FIRSTLINE;
 
 	//Write X:
-	sprintf(StringToDisplay, "gX: %li      ", gX);
-	ssd1306_SetCursor(60, currentLine);
+	sprintf(StringToDisplay, "gX:%li      ", gX);
+	ssd1306_SetCursor(SECONDCOLUMN, currentLine);
 	ssd1306_WriteString(StringToDisplay, SMALLFONT, White);
 	currentLine += LINESPACE;
 
 	//Write Y:
-	sprintf(StringToDisplay, "gY: %li      ", gY);
-	ssd1306_SetCursor(60, currentLine);
+	sprintf(StringToDisplay, "gY:%li      ", gY);
+	ssd1306_SetCursor(SECONDCOLUMN, currentLine);
 	ssd1306_WriteString(StringToDisplay, SMALLFONT, White);
 	currentLine += LINESPACE;
 
 	//Write Z:
-	sprintf(StringToDisplay, "gZ: %li      ", gZ);
-	ssd1306_SetCursor(60, currentLine);
+	sprintf(StringToDisplay, "gZ:%li      ", gZ);
+	ssd1306_SetCursor(SECONDCOLUMN, currentLine);
 	ssd1306_WriteString(StringToDisplay, SMALLFONT, White);
 
 	//Update screen
 	ssd1306_UpdateScreen();
+}
+
+void displayRawValues (int32_t aX, int32_t gX, int32_t aY, int32_t gY, int32_t aZ, int32_t gZ)
+{
+	char StringToDisplay[128];
+	uint8_t currentLine = FIRSTLINE;
+
+	//Write X:
+	sprintf(StringToDisplay, "aX:%li     ", aX);
+	ssd1306_SetCursor(0, currentLine);
+	ssd1306_WriteString(StringToDisplay, SMALLFONT, White);
+
+	//Write X:
+	sprintf(StringToDisplay, "gX:%li      ", gX);
+	ssd1306_SetCursor(SECONDCOLUMN, currentLine);
+	ssd1306_WriteString(StringToDisplay, SMALLFONT, White);
+
+	currentLine += LINESPACE;
+
+	//Write Y:
+	sprintf(StringToDisplay, "aY:%li    ", aY);
+	ssd1306_SetCursor(0, currentLine);
+	ssd1306_WriteString(StringToDisplay, SMALLFONT, White);
+
+	//Write Y:
+	sprintf(StringToDisplay, "gY:%li      ", gY);
+	ssd1306_SetCursor(SECONDCOLUMN, currentLine);
+	ssd1306_WriteString(StringToDisplay, SMALLFONT, White);
+
+	currentLine += LINESPACE;
+
+	//Write Z:
+	sprintf(StringToDisplay, "aZ:%li     ", aZ);
+	ssd1306_SetCursor(0, currentLine);
+	ssd1306_WriteString(StringToDisplay, SMALLFONT, White);
+
+	//Write Z:
+	sprintf(StringToDisplay, "gZ:%li      ", gZ);
+	ssd1306_SetCursor(SECONDCOLUMN, currentLine);
+	ssd1306_WriteString(StringToDisplay, SMALLFONT, White);
+
+	//Update screen
+	ssd1306_UpdateScreen();
+
 }
 
