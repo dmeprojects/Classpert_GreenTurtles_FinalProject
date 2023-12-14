d7b0aee26f3e392ee04a5f1aef41a2a5ea29210d
DME-Projects <79807855+dmeprojects@users.noreply.github.com>
2023-12-14 20:58:35 +0100

    Fixed script with correct links

--- Final project/Firmware/Application/Inc/version.h
+++ Final project/Firmware/Application/Inc/version.h
@@ -1,10 +1,10 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_STRING "Console 0.0.1.0"
+#define VERSION_STRING "Aim-A-Lyzer"
 
 #define FIRMWARE_MAJOR 0
 #define FIRMWARE_MINOR 0
-#define FIRMWARE_BUILD 0
+#define FIRMWARE_BUILD 9
 
 #endif // VERSION_H
--- Scripts/IncrementVersion.py
+++ Scripts/IncrementVersion.py
@@ -2,6 +2,7 @@ import os
 import re
 import json
 
+print("Auto version incrementor")
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 PROJECT_DIR = os.path.dirname(SCRIPT_DIR)
@@ -9,10 +10,6 @@ VERSION_HEADER_DIR = os.path.join(PROJECT_DIR, "Final project", "Firmware", "App
 VERSION_INFO_FILE = os.path.join(SCRIPT_DIR, "version_info.json")
 
 #Remove the script 
-
-
-print(VERSION_HEADER_DIR)
-print (VERSION_INFO_FILE)
 
 def load_version_info():
     try:
@@ -54,6 +51,9 @@ def update_version_header(file_path):
     else:
         # Set default minor version if the pattern is not found
         current_minor_version = 0
+    
+    print("Version Info:")
+    print("Current Version " + str(current_major_version) + " . " + str(current_minor_version) + " . " + str(current_build))
 
     # Load old version information
     old_version_info = load_version_info()
@@ -80,6 +80,8 @@ def update_version_header(file_path):
     new_content = re.sub(r'#define FIRMWARE_BUILD (\d+)', f'#define FIRMWARE_BUILD {new_build_number}', content)
     new_content = re.sub(r'#define FIRMWARE_MINOR (\d+)', f'#define FIRMWARE_MINOR {new_minor_version}', new_content)
     
+    print("New content: " + new_content)
+    
     
     # Write the updated content back to the version.h file
     with open(file_path, 'w') as file:
@@ -89,6 +91,7 @@ def update_version_header(file_path):
     new_version_info = {"major": current_major_version, "minor": new_minor_version, "build": new_build_number}
     save_version_info(new_version_info)
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     version_header_path = os.path.join(VERSION_HEADER_DIR,"version.h")
+    print("Header file location: " + version_header_path)
     update_version_header(version_header_path)
